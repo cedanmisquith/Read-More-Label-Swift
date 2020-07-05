336e87ce99649d6ab766d48dd457a3f3dacac1a6
Cedan Misquith <cedanmisquith@gmail.com>
2020-07-06 02:41:21 +0530

    Initial Commit

---
+++ ReadMoreLabel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadMoreLabel
+//
+//  Created by Cedan Misquith on 06/07/20.
+//  Copyright © 2020 Cedan Misquith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
